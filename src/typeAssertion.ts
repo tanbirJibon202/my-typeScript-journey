a236464b53cb10af9b762b77dee0e9abcee753db
tanbirJibon202 <tanbirahamed202@gmail.com>
2026-05-03 00:54:12 +0600

    typeAssertion Concept

---
+++ src/typeAssertion.ts
@@ -0,0 +1,28 @@
+let anything: any;
+anything = "Tanbir";
+anything as number;
+
+const kgToGMConverter = (
+  input: string | number,
+): string | number | undefined => {
+  if (typeof input === "number") {
+    return input * 1000;
+  } else if (typeof input === "string") {
+    const [value] = input.split(" ");
+    return `Converted output is : ${Number(value) * 1000}`;
+  }
+};
+
+const result1 = kgToGMConverter(10) as number;
+console.log({ result1 });
+const result2 = kgToGMConverter("2 KG") as string;
+console.log({ result2 });
+
+type CustomError = {
+  message: string;
+};
+
+try {
+} catch (err) {
+  console.log((err as CustomError).message);
+}
