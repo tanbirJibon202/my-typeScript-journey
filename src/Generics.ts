108d4bf30ca89eaf5ca12b0b5d0e306d4d96e3f3
tanbirJibon202 <tanbirahamed202@gmail.com>
2026-05-03 13:50:34 +0600

    Generics Concept

---
+++ src/Generics.ts
@@ -0,0 +1,31 @@
+// dynamically generalize: Generics
+// const friends : string[] = ['Alice', 'Bob', 'Charlie'];
+
+type GenericArray<value> = Array<value>;
+const friends: GenericArray<string> = ["Alice", "Bob", "Charlie"];
+const rollNumbers: GenericArray<number> = [1, 2, 3, 4, 5];
+const isElligibleList: GenericArray<boolean> = [true, false, true, true];
+
+// const sqrFunc = (value : number) : number => value * value;
+// console.log(sqrFunc(5));
+
+// const sqrFunc = (value: number) =>{
+//   return value * value;
+// }
+// sqrFunc(8);
+
+type Cordinates<X, Y> = [X, Y];
+const cordinates_1: Cordinates<string, string> = ["20","30"];
+const cordinates_2: Cordinates<number, number> = [20,30];
+
+
+const userList: GenericArray<{ name: string; age: number }> = [
+  {
+  name: "Alice",
+  age: 25,
+  },
+  {
+  name: "Bob",
+  age: 30,
+  }
+]
